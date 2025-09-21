601e627f592efe22acb506f91e0e758056b0b54f
Vikash Upadhyay <vikashpk761@gmail.com>
2025-09-21 21:34:49 +0530

    Added arrow key to active and deative the image

--- js/index.js
+++ js/index.js
@@ -1,14 +1,40 @@
 const panels=document.querySelectorAll(".panel");
+const len=panels.length;
 
 panels.forEach(panel=>{
     panel.addEventListener("click",event=>{
-        deslect();
-        panel.classList.add("active")
+        activePanel(panel);
+    })
+    panel.addEventListener("keydown",event =>{
+        if(event.key === "ArrowRight"){
+            navigateNext();
+        }
+        else if(event.key ==="ArrowLeft"){
+            navigatePrev();
+        }
     })
 })
+
+function navigateNext() {
+    const currentIndex = Array.from(panels).findIndex(p => p.classList.contains("active"));
+    const requiredIndex = (currentIndex + 1) % len;
+    activePanel(panels[requiredIndex]);
+}
+
+function navigatePrev() {
+    const currentIndex = Array.from(panels).findIndex(p => p.classList.contains("active"));
+    const requiredIndex = (currentIndex - 1 + len) % len;
+    activePanel(panels[requiredIndex]);
+}
 
 function deslect(){
     panels.forEach(panel=>{
         panel.classList.remove("active");
     })
+}
+
+function activePanel(p){
+    deslect();
+    p.classList.add("active");
+    p.focus();
 }
